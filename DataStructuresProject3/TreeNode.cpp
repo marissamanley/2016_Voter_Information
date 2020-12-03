c93481f275b70df71cfb130bc967ad49646ea415
Mitkin-00 <58479474+Mitkin-00@users.noreply.github.com>
2020-12-02 22:23:11 -0500

    Added copy constructor and TreeNode class

---
+++ DataStructuresProject3/TreeNode.cpp
@@ -0,0 +1,17 @@
+#include "TreeNode.h"
+
+TreeNode::TreeNode()
+{
+}
+
+TreeNode::TreeNode(States s)
+{
+	state = s;
+}
+
+TreeNode::TreeNode(States s, TreeNode* left, TreeNode* right)
+{
+	state = s;
+	this->left = left;
+	this->right = right;
+}
